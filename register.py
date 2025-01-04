795f2e880ff19f384773ad42c7dfd6920231793d
Han <59479453+recta-rrae@users.noreply.github.com>
2025-01-04 21:18:23 +0700

    feat : add advance validation algorithm for email regist (complete)

--- register.py
+++ register.py
@@ -13,8 +13,68 @@ conn = mysql.connector.connect(
 )
 cursor = conn.cursor()
 
-def valid_email(email):
-    return email.endswith('@gmail.com')
+def email_validation_algorithm(email):
+    e_arr = []
+
+    atsym_count = 0
+    atsym_pass = False
+
+    dot_count = 0
+    dot_pass = False
+
+    is_valid = None
+
+    # Ubah text email menjadi sebuah array, dengan elemennya huruf atau angka
+    for i in range(len(email)):
+        e_arr.append(email[i])
+
+    # Check apakah ada symbol @?
+    for i in range(len(e_arr)):
+        if e_arr[i] == "@":
+            atsym_count += 1
+        else:
+            if i == (len(e_arr) - 1):
+                atsym_pass = False
+                break
+            else:
+                continue
+
+    # Jika '@' ada 1, maka valid. Lebih dari 1 atau kurang dari 1, maka tidak valid.
+    atsym_pass = atsym_count == 1
+
+    # Check apakah ada domain name?
+    for i in range(len(e_arr)):
+        if e_arr[i] == ".":
+            dot_count += 1
+        else:
+            if i == (len(e_arr) - 1):
+                dot_pass = False
+            else:
+                continue
+
+    # Jika '.' ada lebih dari 1, maka valid. Jika tidak ada, maka tidak valid.
+    dot_pass = dot_count >= 1
+
+    # Bagi menjadi 2 bagian email, @ ke kiri, dan @ ke kanan.
+    if (atsym_pass and dot_pass):
+        em_split = email.split("@")
+        kiri_at = em_split[0]
+        kanan_at = em_split[1]
+        domain = kanan_at.split('.')
+
+        # Check panjang masing2 subdomain dan domain
+        # Jika lebih dari 1, maka valid. Jika kurang, maka tidak valid.
+        for i in domain:
+            if len(i) >= 1:
+                is_valid = True
+                continue
+            else:
+                is_valid = False
+                break
+    else:
+        is_valid = False
+
+    return is_valid
 
 def register_mahasiswa():
     print("\n=== Register Mahasiswa ===")
@@ -31,13 +91,17 @@ def register_mahasiswa():
     if nim == 0:
         return
     
-    # Validasi Email kosong
+    # Validasi Email
     while True:
         email = input("Masukkan Email: ").strip().lower()
         if len(email) <= 0:
-            print("Email tidak boleh kosong")
+            print("Email tidak boleh kosong.")
         else:
-            break
+            email_valid = email_validation_algorithm(email)
+            if (email_valid == True):
+                break
+            else:
+                print("Email tidak valid!")
         
     
     # Validasi Password kosong
@@ -75,7 +139,11 @@ def register_admin():
         if len(email) <= 0:
             print("Email tidak boleh kosong.")
         else:
-            break
+            email_valid = email_validation_algorithm(email)
+            if (email_valid == True):
+                break
+            else:
+                print("Email tidak valid!")
     # Validasi Password kosong
     while True:
         password = input("Masukkan Password: ").strip()
